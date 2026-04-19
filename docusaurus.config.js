04e4383043ba22f8a0254b561fbef1e0544634a4
Janeera-DA <141397958+Janeera-DA@users.noreply.github.com>
2026-04-19 21:53:41 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,6 +13,7 @@ const config = {
   organizationName: 'janeera-da',
   projectName: 'Portfolio',
   trailingSlash: false,
+  future: {}, 
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
